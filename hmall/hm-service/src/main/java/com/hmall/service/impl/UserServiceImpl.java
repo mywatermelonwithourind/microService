05f2a68851cd7227b0a2217fb1120515f75673ba
itcast <1148537112@qq.com>
2026-02-03 14:14:14 +0800

    登录功能完善

--- hmall/hm-service/src/main/java/com/hmall/service/impl/UserServiceImpl.java
+++ hmall/hm-service/src/main/java/com/hmall/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.hmall.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmall.common.exception.BadRequestException;
 import com.hmall.common.exception.BizIllegalException;
@@ -19,6 +21,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.time.LocalTime;
+
 /**
  * <p>
  * 用户表 服务实现类
@@ -34,11 +38,43 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     private final JwtTool jwtTool;
 
     private final JwtProperties jwtProperties;
+    private final UserMapper userMapper;
 
     @Override
     public UserLoginVO login(LoginFormDTO loginDTO) {
-        //TODO 校验用户名和密码; 用户名、密码不对登录失败；状态为冻结不能登录；验证成功后生成token
-        return null;
+
+
+        // 1. 构造查询条件：只根据用户名查询（不要在 SQL 里比对密码）
+        User user = lambdaQuery()
+                .eq(User::getUsername, loginDTO.getUsername())
+                .one();
+
+        // 2. 校验用户是否存在
+        if (user == null) {
+            throw new BadRequestException("用户名或密码错误");
+        }
+
+        // 3. 优化：先校验状态 (轻量级判断，保护 CPU)
+        if (user.getStatus() == UserStatus.FROZEN) { //
+            throw new BadRequestException("用户状态被冻结，禁止登录");
+        }
+
+        // 4. 再校验密码 (重量级计算)
+        if (!passwordEncoder.matches(loginDTO.getPassword(), user.getPassword())) {
+            throw new BadRequestException("用户名或密码错误");
+        }
+
+        //5.根据id生成令牌
+        String token = jwtTool.createToken(user.getId(), jwtProperties.getTokenTTL());
+
+        // 6. 封装返回结果 UserLoginVO
+        UserLoginVO vo = new UserLoginVO();
+        vo.setUserId(user.getId());
+        vo.setUsername(user.getUsername());
+        vo.setBalance(user.getBalance());
+        vo.setToken(token); // 设置生成的 token
+
+        return vo;
     }
 
     @Override
