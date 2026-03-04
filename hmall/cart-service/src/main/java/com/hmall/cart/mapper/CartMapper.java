e09ae3d746fd1b45a12999cdcacd18adc4ea9228
itcast <1148537112@qq.com>
2026-03-04 23:55:59 +0800

    微服务拆分（未服务调用版）--商品以及购物车部分

---
+++ hmall/cart-service/src/main/java/com/hmall/cart/mapper/CartMapper.java
@@ -0,0 +1,15 @@
+package com.hmall.cart.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hmall.cart.domain.po.Cart;
+
+/**
+ * <p>
+ * 订单详情表 Mapper 接口
+ * </p>
+ *
+ */
+public interface CartMapper extends BaseMapper<Cart> {
+
+   
+}
---
+++ hmall/item-service/src/main/java/com/hmall/item/controller/SearchController.java
@@ -0,0 +1,64 @@
+package com.hmall.item.controller;
+
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmall.common.domain.PageDTO;
+import com.hmall.item.domain.dto.ItemDTO;
+import com.hmall.item.domain.po.Item;
+import com.hmall.item.domain.query.ItemPageQuery;
+import com.hmall.item.service.IItemService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "搜索相关接口")
+@RestController
+@RequestMapping("/search")
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final IItemService itemService;
+
+    @ApiOperation("搜索商品")
+    @GetMapping("/list")
+    public PageDTO<ItemDTO> search(ItemPageQuery query) {
+        // 1. 构建分页对象
+        Page<Item> page = new Page<>(query.getPageNo(), query.getPageSize());
+
+        // 2. 链式构建查询条件
+        LambdaQueryChainWrapper<Item> wrapper = itemService.lambdaQuery()
+                // 业务硬性指标：必须是上架商品 (Fix 漏洞)
+                .eq(Item::getStatus, 1) //
+                // 模糊搜索
+                .like(StrUtil.isNotBlank(query.getKey()), Item::getName, query.getKey())
+                // 过滤条件
+                .eq(StrUtil.isNotBlank(query.getCategory()), Item::getCategory, query.getCategory())
+                .eq(StrUtil.isNotBlank(query.getBrand()), Item::getBrand, query.getBrand())
+                // 价格区间
+                .ge(query.getMinPrice() != null, Item::getPrice, query.getMinPrice())
+                .le(query.getMaxPrice() != null, Item::getPrice, query.getMaxPrice());
+
+        // 3. 处理排序
+        String sortBy = query.getSortBy();
+        if ("price".equals(sortBy)) {
+            wrapper.orderBy(true, query.getIsAsc(), Item::getPrice);
+        } else if ("sold".equals(sortBy)) {
+            wrapper.orderBy(true, query.getIsAsc(), Item::getSold);
+        } else {
+            // 默认按更新时间降序
+            wrapper.orderByDesc(Item::getUpdateTime);
+        }
+
+        // 4. 执行查询
+        Page<Item> result = wrapper.page(page);
+
+        // 5. 返回
+        return PageDTO.of(result, ItemDTO.class);
+
+    }
+}
