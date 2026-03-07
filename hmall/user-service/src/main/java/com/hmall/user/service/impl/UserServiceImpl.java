9b68c71cd51783834e3f246181e463c99e972f87
itcast <1148537112@qq.com>
2026-03-07 16:55:51 +0800

    用户模块微服务拆分

---
+++ hmall/user-service/src/main/java/com/hmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,92 @@
+package com.hmall.user.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.common.exception.BadRequestException;
+import com.hmall.common.exception.BizIllegalException;
+import com.hmall.common.utils.UserContext;
+import com.hmall.user.config.JwtProperties;
+import com.hmall.user.domain.dto.LoginFormDTO;
+import com.hmall.user.domain.po.User;
+import com.hmall.user.domain.vo.UserLoginVO;
+import com.hmall.user.enums.UserStatus;
+import com.hmall.user.mapper.UserMapper;
+import com.hmall.user.service.IUserService;
+import com.hmall.user.utils.JwtTool;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户表 服务实现类
+ * </p>
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtTool jwtTool;
+
+    private final JwtProperties jwtProperties;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserLoginVO login(LoginFormDTO loginDTO) {
+
+
+        // 1. 构造查询条件：只根据用户名查询（不要在 SQL 里比对密码）
+        User user = lambdaQuery()
+                .eq(User::getUsername, loginDTO.getUsername())
+                .one();
+
+        // 2. 校验用户是否存在
+        if (user == null) {
+            throw new BadRequestException("用户名或密码错误");
+        }
+
+        // 3. 优化：先校验状态 (轻量级判断，保护 CPU)
+        if (user.getStatus() == UserStatus.FROZEN) { //
+            throw new BadRequestException("用户状态被冻结，禁止登录");
+        }
+
+        // 4. 再校验密码 (重量级计算)
+        if (!passwordEncoder.matches(loginDTO.getPassword(), user.getPassword())) {
+            throw new BadRequestException("用户名或密码错误");
+        }
+
+        //5.根据id生成令牌
+        String token = jwtTool.createToken(user.getId(), jwtProperties.getTokenTTL());
+
+        // 6. 封装返回结果 UserLoginVO
+        UserLoginVO vo = new UserLoginVO();
+        vo.setUserId(user.getId());
+        vo.setUsername(user.getUsername());
+        vo.setBalance(user.getBalance());
+        vo.setToken(token); // 设置生成的 token
+
+        return vo;
+    }
+
+    @Override
+    public void deductMoney(String pw, Integer totalFee) {
+        log.info("开始扣款");
+        // 1.校验密码
+        User user = getById(UserContext.getUser());
+        if(user == null || !passwordEncoder.matches(pw, user.getPassword())){
+            // 密码错误
+            throw new BizIllegalException("用户密码错误");
+        }
+
+        // 2.尝试扣款
+        try {
+            baseMapper.updateMoney(UserContext.getUser(), totalFee);
+        } catch (Exception e) {
+            throw new RuntimeException("扣款失败，可能是余额不足！", e);
+        }
+        log.info("扣款成功");
+    }
+}
